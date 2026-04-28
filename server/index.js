ea12f20742104509de1811f1eac476f4a2b960d9
Ganesh Kumar <gk044996@gmail.com>
2026-04-28 23:52:51 +0530

    fixed useEffect + final build

--- server/index.js
+++ server/index.js
@@ -411,6 +411,7 @@ app.get("/history/:email", async (req, res) => {
     } catch (err) { res.status(500).json("Server error"); }
 });
 
-app.listen(5000, () => {
-    console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
